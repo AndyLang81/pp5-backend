584eac3ab8f402b7eb577a35c45a40b59d8d4a2e
AndyLang81 <Andylanghoff@gmail.com>
2025-05-24 23:38:20 +0200

    Enable DEBUG config via environment variable

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Security settings
 SECRET_KEY = os.getenv("SECRET_KEY")
-DEBUG = False
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 ALLOWED_HOSTS = ['pp5-backend.onrender.com', 'localhost', '127.0.0.1']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
